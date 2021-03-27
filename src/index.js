b2580615c2e7f6ab003a1942748fb007b7c2870a
Rei Krom <mr.mygo@gmail.com>
2021-03-27 18:21:59 +0000

    add redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { addArticle } from './redux/actions';
+
+const rootElement = document.getElementById('root');
+window.store = store;
+window.addArticle = addArticle;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
