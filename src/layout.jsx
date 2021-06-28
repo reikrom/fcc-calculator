60300cf8ec24db1154b2341ea90953ed3507c7b4
Rei Krom <mr.mygo@gmail.com>
2021-06-28 22:26:07 +0100

    remove sin

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,37 +2,36 @@ import React from 'react';
 import './layout.scss';
 import { buttons } from './buttons';
 
-const Layout = ({ output, tempVal, handleClick, warning }) => {
-  let current = tempVal.length === 0 ? '0' : tempVal;
-  if (warning) {
-    current = 'DIGIT LIMIT REACHED';
-  }
+const Layout = ({ output, tempVal, handleClick, warning, history }) => {
+	let current = tempVal.length === 0 ? '0' : tempVal;
+	if (warning) {
+		current = 'DIGIT LIMIT REACHED';
+	}
 
-  return (
-    <div className="calculator">
-      <div id="display">
-        <div className="calculations">{output}</div>
-        <div className="current result">{current}</div>
-      </div>
-      <div className="tabs bred">
-        <div className="history nonselect">History</div>
-        <div className="sin nonselect">Sin</div>
-      </div>
-      <div className="buttonWrapper nonselect">
-        {buttons.map((b) => (
-          <button
-            onClick={(e) => handleClick(e)}
-            className="button"
-            id={b.id}
-            key={b.id}
-            value={b.value}
-          >
-            {b.value}
-          </button>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="calculator">
+			<div id="display">
+				<div className="calculations">{output}</div>
+				<div className="current result">{current}</div>
+			</div>
+			<div className="tabs">
+				<div className="history nonselect">History</div>
+			</div>
+			<div className="buttonWrapper nonselect">
+				{buttons.map((b) => (
+					<button
+						onClick={(e) => handleClick(e)}
+						className="button"
+						id={b.id}
+						key={b.id}
+						value={b.value}
+					>
+						{b.value}
+					</button>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
